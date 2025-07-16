dde9e676bba434a083749ef2a97e52e38461c8b0
Evangeline-O <evangelineomuko@gmail.com>
2025-07-16 08:20:02 +0300

    task2

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -107,7 +107,8 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import UserProfile from './components/UserProfile'; // Added
+import UserProfile from './components/UserProfile'; 
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -122,6 +123,7 @@ function App() {
         age={25} 
         bio="Loves hiking and photography" 
       />
+      <Counter />
       
       <WelcomeMessage />
       <div>
